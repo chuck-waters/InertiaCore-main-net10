ee302896a57099e43837fc147fe88b27be362b5a
Austin Drummond <adrum0x7@gmail.com>
2025-09-20 22:17:55 -0400

    persist errors on redirect

--- InertiaCore/Utils/BackResult.cs
+++ InertiaCore/Utils/BackResult.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using InertiaCore.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace InertiaCore.Utils;
 
@@ -12,6 +14,14 @@ public class BackResult : IActionResult
 
     public async Task ExecuteResultAsync(ActionContext context)
     {
+        // Store validation errors in TempData if ModelState has errors
+        if (!context.ModelState.IsValid)
+        {
+            var tempDataFactory = context.HttpContext.RequestServices.GetRequiredService<ITempDataDictionaryFactory>();
+            var tempData = tempDataFactory.GetTempData(context.HttpContext);
+            tempData.SetValidationErrors(context.ModelState);
+        }
+
         var referrer = context.HttpContext.Request.Headers.Referer.ToString();
         var redirectUrl = !string.IsNullOrEmpty(referrer) ? referrer : _fallbackUrl;
 
