e0c2689f18c622923527150c6e806a6aa58a2633
Austin Drummond <adrum0x7@gmail.com>
2025-09-21 18:49:34 -0400

    change back http status to 303

--- InertiaCore/Utils/BackResult.cs
+++ InertiaCore/Utils/BackResult.cs
@@ -7,14 +7,19 @@ namespace InertiaCore.Utils;
 public class BackResult : IActionResult
 {
     private readonly string _fallbackUrl;
+    private readonly HttpStatusCode _statusCode;
 
-    public BackResult(string? fallbackUrl = null) => _fallbackUrl = fallbackUrl ?? "/";
+    public BackResult(string? fallbackUrl = null, HttpStatusCode statusCode = HttpStatusCode.SeeOther) =>
+        (_fallbackUrl, _statusCode) = (fallbackUrl ?? "/", statusCode);
 
-    public async Task ExecuteResultAsync(ActionContext context)
+    public Task ExecuteResultAsync(ActionContext context)
     {
         var referrer = context.HttpContext.Request.Headers.Referer.ToString();
         var redirectUrl = !string.IsNullOrEmpty(referrer) ? referrer : _fallbackUrl;
 
-        await new RedirectResult(redirectUrl).ExecuteResultAsync(context);
+        context.HttpContext.Response.StatusCode = (int)_statusCode;
+        context.HttpContext.Response.Headers.Location = redirectUrl;
+
+        return Task.CompletedTask;
     }
 }
